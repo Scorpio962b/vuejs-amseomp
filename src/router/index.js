f2acbda33befacd187182d16c4db1341957370be
Scorpio962b <samaaiab962@gmail.com>
2023-07-05 10:59:11 +0200

    projects only

--- src/router/index.js
+++ src/router/index.js
@@ -3,35 +3,11 @@ import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/AboutView.vue')
-  },
-  {
-    path: '/testimonial',
-    name: 'testimonial',
-    component: () => import('../views/testimonial.vue'),
-  },
-  {
-    path: '/ContactMe',
-    name: 'ContactMe',
-    component: () => import('../views/ContactMe.vue'),
-  },
-  {
     path: '/Projects',
     name: 'Projects',
     component: () => import('../views/Projects.View.vue'),
   },
-  {
-    path: '/resume',
-    name: 'resume',
-    component: () => import('../views/resumeView.vue'),
-  },
+ 
 ]
 
 const router = createRouter({
