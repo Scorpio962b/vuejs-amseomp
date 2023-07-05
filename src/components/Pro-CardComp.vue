39c3384e351fe122cec1f2ebc3674635391c4480
Scorpio962b <samaaiab962@gmail.com>
2023-07-05 10:18:04 +0200

    projects

---
+++ src/components/Pro-CardComp.vue
@@ -0,0 +1,22 @@
+<template>
+    <router-link  :to="{name:'project'}">:
+    <img :src="project.imageUrl" :alt="project.name">
+    <h3>{{project.name}}
+    </h3>   
+    </router-link>
+</template>
+<script>
+
+export default {
+    props:["project"]
+}
+</script>
+
+<style scoped>
+  body{
+    height:60px;
+    width:60px
+  }
+ 
+
+</style>
