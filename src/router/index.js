af20528cace3a5294fe18aed6a66ab8bff4a6bc6
Scorpio962b <samaaiab962@gmail.com>
2023-07-05 10:52:57 +0200

    testimonialsonly

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/AboutView.vue')
-  },
+const routes = 
   {
     path: '/testimonial',
     name: 'testimonial',
     component: () => import('../views/testimonial.vue'),
-  },
-  {
-    path: '/ContactMe',
-    name: 'ContactMe',
-    component: () => import('../views/ContactMe.vue'),
-  },
-  {
-    path: '/Projects',
-    name: 'Projects',
-    component: () => import('../views/Projects.View.vue'),
-  },
-  {
-    path: '/resume',
-    name: 'resume',
-    component: () => import('../views/resumeView.vue'),
-  },
-]
+  }
+ 
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
